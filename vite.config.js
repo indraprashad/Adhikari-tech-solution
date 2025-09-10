776eb16292b97060077aa274ad48ada7c674930c
Indra Sharma <indra.sharma@dcpl.bt>
2025-09-11 02:28:18 +0600

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/Adhikari-tech-solution/",
   plugins: [react()],
   resolve: {
     alias: {
